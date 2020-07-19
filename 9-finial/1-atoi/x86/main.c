921a10342c3344f0b73f390f88212a1953d28570
cebarobot <ceba_robot@outlook.com>
2020-07-19 14:00:55 +0800

    finish: Final Assignment - atoi x86

---
+++ 9-finial/1-atoi/x86/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int atoi(const char *nptr);
+
+char str_set[][100] = {
+    "123456789",
+    "-123456789",
+    "1978UCAS2020",
+    "    2020UCAS~",
+    "UCAS welcome~",
+    "1346792580852976431",
+    "-1346792580852976431",
+    "    \t\t-1346792580852976431UCAS",
+    "    \t\t134679UCAS",
+    ""
+};
+
+int main() {
+    printf("Testing int atoi(const char *nptr) ...\n");
+    for (int i = 0; i < 10; i++) {
+        printf("\"%s\" : %d\n", str_set[i], atoi(str_set[i]));
+    }
+    return 0;
+}
